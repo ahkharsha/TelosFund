53c994a258fc14c42c84f8645ec371ab63f5bc64
ahkharsha <ahkharsha@gmail.com>
2025-01-27 22:14:34 +0530

    Contracts

---
+++ contract/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "https://rpc.sepolia.mantle.xyz";
+const NEXT_PUBLIC_PRIVATE_KEY =
+  "";
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "mantle",
+  networks: {
+    hardhat: {},
+    polygon_amoy: {
+      url: NEXT_PUBLIC_POLYGON_MUMBAI_RPC,
+      accounts: [`0x${NEXT_PUBLIC_PRIVATE_KEY}`],
+    },
+  },
+};
